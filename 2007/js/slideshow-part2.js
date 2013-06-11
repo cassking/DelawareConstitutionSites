520a9cd254db737f86f2f8ff19b195d58758aca6
Cassandra King <puddincat@gmail.com>
2013-06-11 17:34:15 -0400

    adding Constitution Day site directories from 2006-2010

---
+++ 2007/js/slideshow-part2.js
@@ -0,0 +1,174 @@
+
+
+SLIDES = new slideshow("SLIDES");
+SLIDES.timeout = 3000;
+SLIDES.prefetch = -1;
+SLIDES.repeat = true;
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case1Document1.jpg";
+s.text = unescape("Sussex%20County%20Petition%20for%20Paper%20Currency%20%28circa%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case1Document2.jpg";
+s.text = unescape("John%20Dickinson%27s%20Report%20from%20the%20Annapolis%20Convention%20%28Sept.%2014%2C%201786%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case1Document3.jpg";
+s.text = unescape("Letter%20from%20Charles%20Thomson%20stating%20support%20of%20Continental%20Congress%20for%20a%20Constitutional%20Convention%20%28February%2021%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case1Document4.jpg";
+s.text = unescape("Act%20of%20the%20Delaware%20House%20of%20Assembly%20to%20nominate%20and%20elect%20Deputies%20to%20attend%20the%20Constitutional%20Convention%20%28February%203%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case2Document1.jpg";
+s.text = unescape("Letter%20from%20Charles%20Thomason%2C%20Secretary%20of%20Congress%2C%20containing%20the%20report%20%28the%20United%20States%20Constitution%29%20from%20the%20Convention%20%u201Clately%20assembled%20in%20Philadelphia.%u201D%20%20%28September%2028%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case2Document2.jpg";
+s.text = unescape("Delaware%20House%20of%20Assembly%20Resolution%20calling%20for%20the%20election%20of%20delegates%20to%20Delaware%u2019s%20Constitutional%20Ratification%20Convention%20%28November%207%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case2Document3.jpg";
+s.text = unescape("A%20Petition%20to%20the%20Delaware%20General%20Assembly%20in%20support%20of%20the%20ratification%20of%20the%20Constitution%20%28October%2024%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case2Document4.jpg";
+s.text = unescape("A%20Petition%20to%20the%20Delaware%20General%20Assembly%20in%20support%20of%20establishing%20the%20Nation%u2019s%20Capital%20in%20Delaware%20%28October%2024%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case3Document1.jpg";
+s.text = unescape("Act%20of%20Congress%20to%20establish%20Government%20under%20the%20new%20Constitution%20%28September%2013%2C%201788%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case3Document2.jpg";
+s.text = unescape("Letter%20from%20Charles%20Thomson%2C%20Secretary%20of%20Congress%2C%20announcing%20an%20Act%20of%20the%20United%20States%20Congress%20to%20put%20the%20operation%20of%20the%20ratified%20Constitution%20into%20operation%20%28September%2013%2C%201788%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case3Document3.jpg";
+s.text = unescape("Receipt%20for%20Expenses%20at%20the%20Ratification%20Convention%20%28December%207%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case3Document4.jpg";
+s.text = unescape("Receipt%20for%20Expenses%20at%20the%20ratification%20Convention%20%28December%207%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case3Document5.jpg";
+s.text = unescape("Receipt%20for%20Expenses%20at%20the%20ratification%20Convention%20%28December%207%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/Case3Document6.jpg";
+s.text = unescape("Auditor%20of%20Accounts%20Waste%20Book%20noting%20state%20government%20expenses%20for%20the%20Ratification%20Convention%20%28December%207%2C%201787%29");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/GeorgeRead2.jpg";
+s.text = unescape("George%20Read");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/GunningBedfordJr.jpg";
+s.text = unescape("Gunning%20Bedford%2C%20Jr.");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/JohnDickinson.jpg";
+s.text = unescape("John%20Dickinson");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+s = new slide();
+s.src =  "http://www.law.widener.edu/constitution/2006/images/slides/RichardBassett.jpg";
+s.text = unescape("Richard%20Bassett");
+s.link = "";
+s.target = "";
+s.attr = "";
+s.filter = "";
+SLIDES.add_slide(s);
+
+
+
+if (false) SLIDES.shuffle();
+
+
